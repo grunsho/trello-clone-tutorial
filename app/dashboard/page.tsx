ef6acec8995ac7841cdbc0acb0a66e2ff32a9045
Álvaro Manterola <alvaro.manterola@gmail.com>
2025-09-10 15:49:05 -0300

    feat: implement dashboard page with navbar and board creation functionality

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import Navbar from '@/components/navbar'
+import { Button } from '@/components/ui/button'
+import { useUser } from '@clerk/nextjs'
+import { Plus } from 'lucide-react'
+
+export default function DashboardPage() {
+  const { user } = useUser()
+  const {createBoards} = useBoards()
+
+  const handleCreateBoard = async () => {
+    await createBoard()
+  }
+
+  return (
+    <div className='min-h-screen bg-gray-50'>
+      <Navbar />
+
+      <main className='container mx-auto px-4 py-6 sm:py-8'>
+        <div className='mb-6 sm:mb-8'>
+          <h1 className='text-2xl sm:text3xl font-bold text-gray-900 mb-2'>
+            Welcome back,{' '}
+            {user?.firstName ?? user?.emailAddresses[0].emailAddress}!👋
+          </h1>
+          <p className='text-gray-600'>
+            Here&apos;s what&apos;s happening with your boards today.
+          </p>
+          <Button className='w-full sm:w-auto' onClick={handleCreateBoard}>
+            <Plus className='h-4 w-4 mr-2' />
+            Create Board
+          </Button>
+        </div>
+      </main>
+    </div>
+  )
+}
